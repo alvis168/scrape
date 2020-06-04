d70d69ec865bc9882661e7b3937f9c8e64612d89
alvis168 <64429523+alvis168@users.noreply.github.com>
2020-06-04 13:14:01 +0700

    Update and rename scrape.py to anoboy-ongoing.py

--- anoboy-ongoing.py
+++ anoboy-ongoing.py
@@ -5,13 +5,13 @@ import json
 page = requests.get("https://anoboy.stream/category/anime/ongoing/")
 soup = BeautifulSoup(page.content, 'html.parser')
 
-quote = soup.find_all('div', class_='column-content')
+quote = soup.find_all('div', class_='amv')
 for q in quote:
-    #x = soup.find_all('div', class_='column-content')
-    #title = q.find ('title').text
-    #link = q.find('a').get('href')
+    x = soup.find('div', class_='column-content')
+    title = q.find ('h3').text
+    link = x.find('a').get('href')
     imagelink = q.find ('amp-img').get('src')
 
-    #print(title)
-    #print(link)
+    print(title)
+    print(link)
     print(imagelink)

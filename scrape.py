6a1af766682c943093d4e718893437cdd85bd7e5
alvis168 <64429523+alvis168@users.noreply.github.com>
2020-06-04 08:39:59 +0700

    Update scrape.py

--- scrape.py
+++ scrape.py
@@ -1,15 +1,17 @@
-
+from bs4 import *
 import requests
 import json
-from bs4 import *
 
-    page=requests.get("https://anoboy.stream/")
-    soup=BeautifulSoup(page.content,'html.parser')
-    lis=soup.find('div',class_='amvj')
-    for q in lis:
-        imagelink=q.find('amp-img').get('src')
-        #animelink=baseurl+li.find('a').get('href')
-        title=q.find('h3').text
-        print(title)
-        #print(animelink)
-        print(imagelink)
+page = requests.get("https://anoboy.stream/category/anime/ongoing/")
+soup = BeautifulSoup(page.content, 'html.parser')
+
+quote = soup.find_all('div', class_='column-content')
+for q in quote:
+    #x = soup.find_all('div', class_='column-content')
+    #title = q.find ('title').text
+    #link = q.find('a').get('href')
+    imagelink = q.find ('amp-img').get('src')
+
+    #print(title)
+    #print(link)
+    print(imagelink)
